c375dcfca25a33eadae0f525f83fb283df2b441e
krutik-dhameliya <37237798+krutik-dhameliya@users.noreply.github.com>
2020-03-04 19:52:30 +0530

    Create paypal.php

---
+++ paypal.php
@@ -0,0 +1,32 @@
+/* inside config */
+
+<?php
+/**
+ * PayPal Setting & API Credentials
+ * Created by Raza Mehdi <srmk@outlook.com>.
+ */
+
+return [
+    'mode'    => 'sandbox', // Can only be 'sandbox' Or 'live'. If empty or invalid, 'live' will be used.
+    'sandbox' => [
+        'username'    => 'ravibi_api1.gmail.com',
+        'password'    => 'BYETZ99HDXQ27V6T',
+        'secret'      => 'AJvnvEoT2SGuCFkUtbT.dLMayxdcApTO8Qgah1VmMxzUl1NuuKIsg.oS',
+        'certificate' => env('PAYPAL_SANDBOX_API_CERTIFICATE', ''),
+        'app_id'      => 'APP-80W284485P519543T', // Used for testing Adaptive Payments API in sandbox mode
+    ],
+    'live' => [
+        'username'    => 'pawan_api1.alliedmarketresearch.com',
+        'password'    => 'KX59S9HYJEASGHZB',
+        'secret'      => 'AndoiyBaSJMQU0x.LA1gzGhgCNPQAByZc5Zw4HrnhfRTOKxz.IXboxLl',
+        'certificate' => env('PAYPAL_LIVE_API_CERTIFICATE', ''),
+        'app_id'      => '', // Used for Adaptive Payments API
+    ],
+
+    'payment_action' => 'Sale', // Can only be 'Sale', 'Authorization' or 'Order'
+    'currency'       => 'USD',
+    'billing_type'   => 'MerchantInitiatedBilling',
+    'notify_url'     => '', // Change this accordingly for your application.
+    'locale'         => '', // force gateway language  i.e. it_IT, es_ES, en_US ... (for express checkout only)
+    'validate_ssl'   => true, // Validate SSL when creating api client.
+];
